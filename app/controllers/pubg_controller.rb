10f095a20d96db00d3298e6fbc0fc1450f6fb4e4
Fulton Zink <joeyzink@gmail.com>
2018-05-12 14:05:54 -0700

    Add route to fetch seasons. Add Pubg class using httparty to make requests to the PUBG API.

--- app/controllers/pubg_controller.rb
+++ app/controllers/pubg_controller.rb
@@ -1,6 +1,22 @@
-class PubgController < ApplicationController
+require 'httparty'
 
-    class PubgController
-        
+class PubgController < ApplicationController
+    def seasons
+        pubg = Pubg.new
+        response = pubg.seasons
+        puts response.body
+    end
+    
+    
+    
+    
+    class Pubg
+        include HTTParty
+        base_uri 'https://api.playbattlegrounds.com/shards/xbox-na'
+        headers "Authorization" => "Bearer #{ENV['PUBG_API_KEY']}", "Accept" => "application/vnd.api+json"
+
+        def seasons
+            self.class.get('/seasons')
+        end
     end
 end
