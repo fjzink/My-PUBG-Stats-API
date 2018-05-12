f98c21ce7d0ee03ac471ab8e3119cea9d6c4c977
Fulton Zink <joeyzink@gmail.com>
2018-05-12 14:54:51 -0700

    Add ability to specify region when querying PUBG API.

--- app/controllers/pubg_controller.rb
+++ app/controllers/pubg_controller.rb
@@ -2,7 +2,8 @@ require 'httparty'
 
 class PubgController < ApplicationController
     def seasons
-        pubg = Pubg.new
+        region = params['region']
+        pubg = Pubg.new(region)
         response = pubg.seasons
         puts response.body
     end
@@ -12,11 +13,15 @@ class PubgController < ApplicationController
     
     class Pubg
         include HTTParty
-        base_uri 'https://api.playbattlegrounds.com/shards/xbox-na'
+        base_uri 'https://api.playbattlegrounds.com/shards'
         headers "Authorization" => "Bearer #{ENV['PUBG_API_KEY']}", "Accept" => "application/vnd.api+json"
 
+        def initialize(region)
+            @region = region
+        end
+
         def seasons
-            self.class.get('/seasons')
+            self.class.get("/#{@region}/seasons")
         end
     end
 end
